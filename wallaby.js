1ad237706fe452d301b306f94eab34cb2aab460e
maierson <dahn@maierinc.com>
2016-11-23 15:55:01 -0500

    one-typescript initial commit

---
+++ wallaby.js
@@ -0,0 +1,35 @@
+module.exports = function (wallaby) {
+    return {
+        files: [
+            'src/**/*.ts'
+        ],
+
+        tests: ['test/**/*test.ts?(x)'],
+
+        compilers: {
+            '**/*.ts': wallaby.compilers.typeScript({
+                module: 'commonjs',// jscs:ignore
+            })
+        },
+
+        testFramework: 'mocha',
+
+        env: {
+            type: 'node'
+        },
+
+        workers: {
+            initial: 1,
+            regular: 1
+        },
+
+        debug: true,
+
+        setup: function () {
+            var jsdom = require('jsdom');
+            global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+            global.window = document.defaultView;
+            global.navigator = global.window.navigator;
+        }
+    };
+};
